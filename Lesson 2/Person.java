0df3a198c09988dde852599da58c1c4415e7f326
max <maxcappie8@gmail.com>
2020-05-20 10:48:02 +0700

    L2 add Person

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name = "Max";
+    char sex = 'M';
+    int age = 25;
+    float height = 1.81f;
+    float weight = 88.8f;
+
+    void move() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void run() {
+
+    }
+
+    String talk() {
+        return "Какая-то фраза на человеческом";
+    }
+
+    boolean learnJava() {
+        return true;
+    }
+}
