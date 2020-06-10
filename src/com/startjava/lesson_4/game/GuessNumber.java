2e9d1677fe7ceeb75a5347d9c8875dc7e4d06ec3
max <maxcappie8@gmail.com>
2020-06-10 16:52:50 +0700

    Lesson 4 add GuessNumber fix

--- src/com/startjava/lesson_4/game/GuessNumber.java
+++ src/com/startjava/lesson_4/game/GuessNumber.java
@@ -1,6 +1,5 @@
 package com.startjava.lesson_4.game;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class GuessNumber {
@@ -10,7 +9,6 @@ public class GuessNumber {
     private Player playerTwo;
     Scanner scanner = new Scanner(System.in);
 
-
     public GuessNumber(int randomNumber, Player playerOne, Player playerTwo) {
         this.randomNumber = randomNumber;
         this.playerOne = playerOne;
@@ -18,66 +16,55 @@ public class GuessNumber {
     }
 
     public void startGame() {
-        makeMove(playerOne);
-        makeMove(playerTwo);
+        playerOne.fillArrayByZeros();
+        playerOne.setAttempt(0);
+        playerTwo.fillArrayByZeros();
+        playerTwo.setAttempt(0);
+        do {
+            if (makeMove(playerOne)) {
+                break;
+            } else if (makeMove(playerTwo)) {
+                break;
+            }
+        } while (true);
         showResult(playerOne);
         showResult(playerTwo);
     }
 
     private boolean makeMove(Player player) {
-        player.setEnteredValues(fillArrayByZeros(player.getEnteredValues()));
-        for (int i = 0; i < 10; i++) {
-            int numberOfAttempts = 10 - i;
+        int numberOfAttempts = 10 - player.getAttempt();
+        if (numberOfAttempts != 0) {
             System.out.print(player.getName() + " [осталось попыток - " + numberOfAttempts + "] введите ваше число: ");
             player.setNumber(scanner.nextInt());
-            player.setEnteredValues(addNumberInArray(i, player.getNumber(), player.getEnteredValues()));
-            if (compareNums(player.getName(), player.getNumber())) {
-                player.setAttempt(++i);
+            player.setEnteredNums(player.getNumber());
+            player.setAttempt(1 + player.getAttempt());
+            if (compareNums(player)) {
                 return true;
             }
+        } else if (numberOfAttempts == 0) {
+            System.out.println("У " + player.getName() + " закончились попытки");
+            return true;
         }
-        System.out.println("У " + player.getName() + " закончились попытки");
         return false;
     }
 
-    private int[] fillArrayByZeros(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 0) {
-                Arrays.fill(array, 0, i, 0);
-                return array;
-            }
-        }
-        return null;
-    }
-
-    private int[] addNumberInArray(int index, int number, int[] array) {
-        array[index] = number;
-        return array;
-    }
-
-    private boolean compareNums(String playerName, int number) {
-        if (number == randomNumber) {
-            System.out.println(playerName + " выйграл(а)!");
+    private boolean compareNums(Player player) {
+        if (player.getNumber() == randomNumber) {
+            System.out.println(player.getName() + " выйграл(а)!");
+            System.out.println("Игрок " + player.getName() + " угадал число " + randomNumber + " с " + player.getAttempt() + " попытки");
             return true;
-        } else if (number < randomNumber) {
-            System.out.println(playerName + " не угадал(а)! Загаданное число больше.");
+        } else if (player.getNumber() < randomNumber) {
+            System.out.println(player.getName() + " не угадал(а)! Загаданное число больше.");
         } else {
-            System.out.println(playerName + " не угадал(а)! Загаданное число меньше.");
+            System.out.println(player.getName() + " не угадал(а)! Загаданное число меньше.");
         }
         return false;
     }
 
     private void showResult(Player player) {
-        if (player.getAttempt() != 0) {
-            System.out.println("Игрок " + player.getName() + " угадал число " + randomNumber + " с " + player.getAttempt() + " попытки");
-        } else {
-            System.out.println("Игрок " + player.getName() + " не угадал число.");
-        }
-        System.out.print("Его попытки: ");
-        for (int index : player.getEnteredValues()) {
-            if (index != 0) {
-                System.out.print(index + " ");
-            }
+        System.out.print("Попытки " + player.getName() + ": ");
+        for (int index : player.getEnteredNums()) {
+            System.out.print(index + " ");
         }
         System.out.println();
     }
--- src/com/startjava/lesson_4/game/Player.java
+++ src/com/startjava/lesson_4/game/Player.java
@@ -6,7 +6,7 @@ public class Player {
 
     private String name;
     private int number;
-    private int[] enteredValues = new int[10];
+    private int[] enteredNums = new int[10];
     private int attempt = 0;
 
     public Player(String name) {
@@ -25,12 +25,17 @@ public class Player {
         this.number = number;
     }
 
-    public int[] getEnteredValues() {
-        return Arrays.copyOf(enteredValues, enteredValues.length);
+    public int[] getEnteredNums() {
+        for (int i = 0; i < enteredNums.length; i++) {
+            if (enteredNums[i] == 0) {
+                return Arrays.copyOf(enteredNums, i);
+            }
+        }
+        return Arrays.copyOf(enteredNums, enteredNums.length);
     }
 
-    public void setEnteredValues(int[] enteredValues) {
-        this.enteredValues = enteredValues;
+    public void setEnteredNums(int enteredNum) {
+        enteredNums[attempt] = enteredNum;
     }
 
     public int getAttempt() {
@@ -39,5 +44,14 @@ public class Player {
 
     public void setAttempt(int attempt) {
         this.attempt = attempt;
+    }
+
+    public void fillArrayByZeros() {
+        for (int i = 0; i < enteredNums.length; i++) {
+            if (enteredNums[i] == 0) {
+                Arrays.fill(enteredNums, 0, i, 0);
+            }
+        }
+        Arrays.fill(enteredNums, 0);
     }
 }
